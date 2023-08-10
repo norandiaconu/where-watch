e3c08fabdffc8c8c9bedca460a836ce412e83228
Noran Diaconu <norandiaconu@gmail.com>
2023-08-10 16:15:37 -0400

    Added Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.norandiaconu.wherewatch',
+  appName: 'where-watch',
+  webDir: 'docs',
+  server: {
+    androidScheme: 'https'
+  },
+  backgroundColor: "#000000"
+};
+
+export default config;
